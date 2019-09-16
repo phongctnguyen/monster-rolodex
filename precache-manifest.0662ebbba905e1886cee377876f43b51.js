40e37d31a5bc245479e10f629310adb16ec152e4
Nguyễn Cao Trường Phong <phong.busbby@gmail.com>
2019-09-16 19:48:13 +0700

    Updates

---
+++ precache-manifest.0662ebbba905e1886cee377876f43b51.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3452df8c8b429618be6c60060aa3645b",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "945950b276351932787f",
+    "url": "/monster-rolodex/static/css/main.e0fcc66c.chunk.css"
+  },
+  {
+    "revision": "0792b5e2043d95efda44",
+    "url": "/monster-rolodex/static/js/2.1159b1ef.chunk.js"
+  },
+  {
+    "revision": "945950b276351932787f",
+    "url": "/monster-rolodex/static/js/main.a681d1d8.chunk.js"
+  },
+  {
+    "revision": "eac7e1388a413f076798",
+    "url": "/monster-rolodex/static/js/runtime~main.f2a7c9f4.js"
+  }
+]);
